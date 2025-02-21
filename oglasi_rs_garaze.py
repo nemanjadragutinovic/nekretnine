0a821db3dad3be131276683bf9c435b14d87a2fa
Nemanja Dragutinovic <nemanjadragutinovic54@gmail.com>
2025-02-22 00:33:23 +0100

    Added garaze.

---
+++ oglasi_rs_garaze.py
@@ -0,0 +1,38 @@
+import time
+import sys
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import telebot
+
+bot = telebot.TeleBot("8116867522:AAFOUlW5xLjPjtdAociSSFLNW66oaxOqxSQ")
+channel_id = "5357149995"
+
+sys.path.append(os.curdir + '\\..')
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get('https://www.oglasi.rs/nekretnine/prodaja-garaza/novi-sad?s=d&rt=vlasnik')
+time.sleep(2)
+
+flat_to_compare = ""
+
+while True:
+    try:
+        flat_list_div = driver.find_element(By.CSS_SELECTOR, 'div[itemtype="http://schema.org/ItemList"]')
+        flat_list = flat_list_div.find_elements(By.CSS_SELECTOR, 'article[itemtype="http://schema.org/Product"]')
+        flat_holder = flat_list[0].find_element(By.CSS_SELECTOR, ".fpogl-holder")
+        flat = flat_holder.find_element(By.CSS_SELECTOR, ".fpogl-list-title")
+        flat_link = flat.get_attribute("href")
+        flat_name = flat.find_element(By.TAG_NAME, "h2").text
+        if flat_to_compare != flat_name:
+            bot.send_message(chat_id=channel_id, text=flat_link)
+            flat_to_compare = flat_name
+    except:
+        print("do nothing")
+    driver.refresh()
+    time.sleep(20)
